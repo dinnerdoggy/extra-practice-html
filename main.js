f40d0dc029ee990d7ca612261dc02eaeac39a83c
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-07-11 13:43:50 -0500

    used js to have a paragraph created using random characters every time

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("connected!")
+let legend = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ",]
+
+let pg = document.getElementById("paragraph")
+pArray = [];
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 37);
+}
+
+
+
+for (let i = 0; i < 1000; i++) {
+  pArray.push(legend[randomWholeNum()])
+}
+
+pg.innerHTML = pArray.join("");
